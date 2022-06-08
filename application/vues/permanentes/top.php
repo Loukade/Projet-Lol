a5b352840183218f44e365e67692b58a6e829a7a
Loukade <lucas.jenvrain@gmail.com>
2022-06-08 18:17:46 +0200

    TOP : problème résolu sur le css pour le controller

--- application/vues/permanentes/top.php
+++ application/vues/permanentes/top.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php if($_REQUEST['Controleur'] == "" || $_REQUEST['Controleur'] == "Affichage" && $_REQUEST['action'] == "afficherSummoner" || $_REQUEST['Controleur'] == "Affichage" && $_REQUEST['action'] == "afficherDetailSummoner" ){ ?>
+    <?php
+
+  if(isset($_REQUEST['Controleur'])){
+    if($_REQUEST['Controleur'] == "" || $_REQUEST['Controleur'] == "Affichage" && $_REQUEST['action'] == "afficherSummoner" || $_REQUEST['Controleur'] == "Affichage" && $_REQUEST['action'] == "afficherDetailSummoner" ){ ?>
         <link rel="stylesheet" href="../../../css/style.css">
     <?php }else{ ?>
     <link rel="stylesheet" href="../../../css/style_champ.css">
-    <?php }?>
+    <?php }
+  }else{
+      ?>
+    <link rel="stylesheet" href="../../../css/style.css">
+    <?php
+  }?>
 
     <title>LoLAnalist</title>
 </head>
