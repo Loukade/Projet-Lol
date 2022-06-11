e5c7aba57b3fd386fa535884dc294bfda6cb49e8
Typofree <sebastiencandido@outlook.fr>
2022-06-11 15:03:03 +0200

    changement texte footer

--- application/vues/permanentes/footer.php
+++ application/vues/permanentes/footer.php
@@ -1,7 +1,7 @@
 
 <footer>
     <h2>A Site powered by frienship</h2>
-    <p class="footer_copiright">© 2012-2022 OP.GG. goLoL.com isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
+    <p class="footer_copiright">© 2012-2022 goLoL.com isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</p>
 </footer>
 
 </html>
