d5ebbd6af8b6cc1dd7e573ee6bd06d5ea867d2da
Typofree <sebastiencandido@outlook.fr>
2022-06-28 23:30:22 +0200

    Ajout De bloc partie en cours et lègere correction de bug

--- application/vues/Summoner/summonerPage.php
+++ application/vues/Summoner/summonerPage.php
@@ -17,7 +17,87 @@
         </div>
     </article>
 </div>
-<section style="height:15vh">
-
+<section class='current_game'>
+    <div class="size_games">
+        <div class="actual_game">
+            <div class="actual_type_game">Ranked Solo / Duo</div>
+            <div class="actual_both_sides">
+                <div class="actual_red_side">
+                    <div class="actual_summoner_red">
+                        <div class="actual_info_red">
+                        <a class="actual_name">Lolathon</a>
+                        <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                    </div>
+                    <div class="actual_summoner_red">
+                        <div class="actual_info_red">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                    </div>
+                    <div class="actual_summoner_red">
+                        <div class="actual_info_red">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                    </div>
+                    <div class="actual_summoner_red">
+                        <div class="actual_info_red">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                    </div>
+                    <div class="actual_summoner_red">
+                        <div class="actual_info_red">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                    </div>
+                </div>
+                <div class="actual_blue_side">
+                    <div class="actual_summoner_blue">
+                    <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                        <div class="actual_info_blue">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                    </div>
+                    <div class="actual_summoner_blue">
+                    <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                        <div class="actual_info_blue">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                    </div>
+                    <div class="actual_summoner_blue">
+                    <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                        <div class="actual_info_blue">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                    </div>
+                    <div class="actual_summoner_blue">
+                    <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                        <div class="actual_info_blue">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                    </div>
+                    <div class="actual_summoner_blue">
+                        <a href="" class="clickable_img"><img class="actual_champ" src="/img/logos.png"></a>
+                        <div class="actual_info_blue">
+                            <a class="actual_name">Lolathon</a>
+                            <div class="actual_mastery">Diamond 3</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
